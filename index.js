ee826ae64065f7f22f4d429fb09694cb5ccc0356
Mashiro Shiina <instinctszsz@gmail.com>
2018-05-12 00:17:12 +0200

    Added feature of reminding me stuff

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const mashiro = new Discord.Client();
+const config = require('./config.json');
+
+mashiro.on('ready', async() => {
+    const presences = {
+        'WATCHING' : 'Jules ♥',
+        'PLAYING' : 'with Jules ♥',
+        'Listening': 'Jules ♥'
+      }
+    
+      setInterval(() => {
+        const types = Object.keys(presences);
+        const type = types[Math.floor(Math.random() * types.length)];
+        mashiro.user.setActivity(presences[type], {type});
+
+      }, 600000);
+
+      console.log("Mashiro woke up~");
+});
+
+mashiro.on('message', (message) => {
+    // Conversation started with Mashiro ♥
+    if (message.content.toLowerCase().startsWith("mashiro")) {
+        const conversation = require("./lib/conversation");
+        conversation.run(mashiro, message);
+    }
+});
+
+
+process.on('unhandledRejection', err => {
+    console.log("Caught unhandledRejection");
+    console.log(err.stack);
+});
+
+mashiro.login(config.token);
